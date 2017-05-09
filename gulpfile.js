10225f7c677acb0fee90e7162086d01d4cd63673
alex <alex@activewidgets.com>
2017-05-09 22:27:27 +0200

    fix sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
 	sourcemaps = require('gulp-sourcemaps'),
-	uglify = require('gulp-uglify');
-
+	uglify = require('gulp-uglify'),
+	version = require('./package.json').version;
 
 var files = [
 	'node_modules/es6-promise/dist/es6-promise.auto.js',
@@ -14,23 +14,28 @@ var files = [
 ];
 
 
+var cfg = {
+	sourceMappingURLPrefix: '/ax-system@' + version + '/dist'
+};
+
+
 gulp.task('source', function() {
 
-	return gulp.src(files)
+	return gulp.src(files, {base: './'})
 		.pipe(sourcemaps.init())
 		.pipe(concat('ax-system.js'))
-		.pipe(sourcemaps.write(''))
+		.pipe(sourcemaps.write('', cfg))
 		.pipe(gulp.dest('dist'));
 });
 
 
 gulp.task('minified', function() {
 
-	return gulp.src(files)
+	return gulp.src(files, {base: './'})
 		.pipe(sourcemaps.init())
 		.pipe(uglify())
 		.pipe(concat('ax-system.min.js'))
-		.pipe(sourcemaps.write(''))
+		.pipe(sourcemaps.write('', cfg))
 		.pipe(gulp.dest('dist'));
 });
 
