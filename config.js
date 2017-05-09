91832910a12fb59490365ce953ef4e1bb9721849
alex <alex@activewidgets.com>
2017-05-09 11:55:06 +0200

    add angular 2, inferno

--- config.js
+++ config.js
@@ -5,12 +5,25 @@ SystemJS.config({
 
 	packages: {
 		'activewidgets': {main: 'dist/ax.js'},
+		'core-js': {main: 'client/shim.min.js'},
+
+		'inferno': {main: 'dist/inferno.js'},
+		'inferno-component': {main: 'dist/inferno-component.js'},
+		'inferno-create-element': {main: 'dist/inferno-create-element.js'},
+
 		'react': {main: 'dist/react.js'},
-		'react-dom': {main: 'dist/react-dom.js'}
+		'react-dom': {main: 'dist/react-dom.js'},
+
+		'rxjs': {main: 'bundles/Rx.min.js'}
 	},
 
 	map: {
 		'activewidgets': 'https://cdn.activewidgets.com/',
+
+		'rxjs/Observable': 'rxjs',
+		'rxjs/observable/merge': '@rxjs.Observable',
+		'rxjs/operator/share': '@rxjs.Observable.prototype',
+		'rxjs/Subject': 'rxjs',
 
 		'css': 'systemjs-plugin-css@0.1.33/css.js',
 		'text': 'systemjs-plugin-text@0.0.9/text.js',
@@ -20,17 +33,24 @@ SystemJS.config({
 	},
 
 	meta: {
+		'@angular/core': {deps: ['core-js', 'zone.js']},
+
 		'*.css': {loader: 'css'},
 		'*.html': {loader: 'text'}
 	},
 
-    babelOptions: {
-        es2015: true,
+	babelOptions: {
+		es2015: true,
 		stage3: true,
 		stage2: false,
 		stage1: false,
 		react: true
-    },
+	},
 
 	transpiler: 'plugin-babel'
 });
+
+
+// @angular hacks
+SystemJS.amdDefine('@rxjs.Observable', ['rxjs'], function(Rx){return Rx.Observable;});
+SystemJS.amdDefine('@rxjs.Observable.prototype', ['rxjs'], function(Rx){return Rx.Observable.prototype;});
