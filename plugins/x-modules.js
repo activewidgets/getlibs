723fafd855289b291a8fae42ed1d7c25a127947a
alex <alex@activewidgets.com>
2017-05-13 23:06:43 +0200

    allow other loaders for x-module content

--- plugins/x-modules.js
+++ plugins/x-modules.js
@@ -12,64 +12,134 @@
 		}
 	}
 
+	var defaultLoader = 'getlibs/loader/default';
+
+	var content = {}, loaders = {'default': defaultLoader};
+
+	function localfetch(load, fetch){
+
+		var name = String(load.name).replace(/!.+$/,'');
+
+		if (content[name]){
+			load.source = content[name];
+			return content[name];
+		}
+
+		var base = Object.getPrototypeOf(this);
+
+		if (base.fetch){
+			return base.fetch.apply(this, arguments);
+		}
+
+		return fetch(load);
+	}
+
+	SystemJS.amdDefine(defaultLoader, [], function(){
+		return {
+			fetch: localfetch
+		};
+	});
+
+
+	function tagLoader(base){
+		var loader = Object.create(base);
+		loader.fetch = localfetch;
+		return loader;
+	};
+
+
+	function registerLoader(name){
+
+		var xname = 'getlibs/loader/' + name,
+			basename = SystemJS.normalizeSync(name),
+			map = {};
+
+		map[name] = xname;
+
+		SystemJS.amdDefine(xname, [basename], tagLoader);
+		SystemJS.config({map: map});
+
+		return xname;
+	}
+
+
+	function makeLoader(name){
+
+		if (!loaders[name]){
+			loaders[name] = registerLoader(name);
+		}
+
+		return loaders[name];
+	}
+
+
+	Object.keys(SystemJS.meta).forEach(function(key){
+
+		var meta = SystemJS.meta[key],
+			name = meta && meta.loader;
+
+		name && makeLoader(name);
+	});
+
+
+	function preloadTranspiler(lang){
+
+		if (lang == 'ts'){
+			SystemJS.import('ts');
+			SystemJS.import('typescript');
+		}
+		else if (!lang && SystemJS.transpiler == 'plugin-babel') {
+			SystemJS.import('plugin-babel');
+			SystemJS.import('systemjs-babel-build');
+		}
+	}
+
+
+	function isDefault(name){
+		return name.match(/\.js$|[^\.\w]\w*$/);
+	}
+
 
 	function registerModules(){
 
-		var content = {};
-
-		function fetch(load){
-			load.source = content[load.name];
-			return content[load.name];
-		}
-
-		SystemJS.registerDynamic('tag-loader', [], false, function(require, exports, module) {
-			exports.fetch = fetch;
-		});
-
-
-		var i, scripts = document.querySelectorAll('script[type="x-module"]');
+		var i, meta = {}, scripts = document.querySelectorAll('script[type="x-module"]');
 
 		for(i=0; i<scripts.length; i++) {
 
 			var el = scripts[i],
 				attr = el.getAttribute('name'),
 				lang = el.getAttribute('lang'),
-				source = el.innerHTML;
-				ext = lang || 'js';
+				src = el.getAttribute('src'),
+				source = el.innerHTML,
+				ext = lang || 'js',
 				name = attr || './script' + (i+1) + '.' + ext,
-				full = SystemJS.normalizeSync(name),
-				meta = {};
+				full = SystemJS.normalizeSync(name);
 
-			if (lang == 'ts') {
-				SystemJS.config({transpiler: 'ts'});
-			}
-
-			if (!attr && source.match(/\bimport\b|\bexport\b/)){
-
-				if (SystemJS.transpiler == 'ts'){
-					SystemJS.import('ts');
-					SystemJS.import('typescript');
-				}
-				else if (SystemJS.transpiler == 'plugin-babel') {
-					SystemJS.import('plugin-babel');
-					SystemJS.import('systemjs-babel-build');
-				}
+			if (src){
+				SystemJS.import(src); // lang?
+				continue;
 			}
 
 			content[full] = source;
 
-			meta[name] = {
-				loader: 'tag-loader'
-			};
+			if (lang=='ts' || (!lang && source.match(/\bimport\b|\bexport\b/))){
+				preloadTranspiler(lang);
+			}
 
-			SystemJS.config({
-				meta: meta
-			});
+			if (isDefault(name)){
+				meta[full] = {
+					loader: defaultLoader
+				};
+			}
 
 			if (!attr){
 				startup.push(name);
 			}
 		}
+
+		SystemJS.config({
+			meta: meta
+		});
 
 		registered = true;
 
