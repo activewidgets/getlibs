15caa4a571a21af1e4d8d3fcd1404dba8ea0b417
alex <alex@activewidgets.com>
2017-05-18 20:13:48 +0200

    more angular packages in config

--- cfg/angular2.js
+++ cfg/angular2.js
@@ -6,15 +6,21 @@ SystemJS.config({
 		'@angular/common': {main: 'bundles/common.umd.min.js'},
 		'@angular/compiler': {main: 'bundles/compiler.umd.min.js'},
 		'@angular/platform-browser': {main: 'bundles/platform-browser.umd.min.js'},
-		'@angular/platform-browser-dynamic': {main: 'bundles/platform-browser-dynamic.umd.min.js'}
+		'@angular/platform-browser-dynamic': {main: 'bundles/platform-browser-dynamic.umd.min.js'},
+		'@angular/forms': {main: 'bundles/forms.umd.min.js'},
+		'@angular/http': {main: 'bundles/http.umd.min.js'},
+		'@angular/router': {main: 'bundles/router.umd.min.js'}
 	},
 
 	map: {
-		'@angular/core': '@angular/core@4.1.2',
-		'@angular/common': '@angular/common@4.1.2',
-		'@angular/compiler': '@angular/compiler@4.1.2',
-		'@angular/platform-browser': '@angular/platform-browser@4.1.2',
-		'@angular/platform-browser-dynamic': '@angular/platform-browser-dynamic@4.1.2'
+		'@angular/core': '@angular/core@4.1.3',
+		'@angular/common': '@angular/common@4.1.3',
+		'@angular/compiler': '@angular/compiler@4.1.3',
+		'@angular/platform-browser': '@angular/platform-browser@4.1.3',
+		'@angular/platform-browser-dynamic': '@angular/platform-browser-dynamic@4.1.3',
+		'@angular/forms': '@angular/forms@4.1.3',
+		'@angular/http': '@angular/http@4.1.3',
+		'@angular/router': '@angular/router@4.1.3'
 	}
 
 });
