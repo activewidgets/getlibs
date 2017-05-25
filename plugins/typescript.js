fbb79c87717e3a4a286d0167634c668eba3a1bd4
alex <alex@activewidgets.com>
2017-05-25 16:57:23 +0200

    fix typescript error reporting

--- plugins/typescript.js
+++ plugins/typescript.js
@@ -6,11 +6,18 @@ SystemJS.amdDefine('getlibs/plugins/typescript', ['getlibs/utils/worker'], funct
 		self.translate = function(address, source, options) {
 
 			/* global ts */
-			return ts.transpileModule(source, {
+			var result = ts.transpileModule(source, {
 				compilerOptions: options,
+				reportDiagnostics: true,
 				moduleName: address,
 				fileName: address
 			});
+
+			result.diagnostics.forEach(function(item){
+				item.file = null;
+			});
+
+			return result;
 		};
 	}
 
@@ -19,8 +26,26 @@ SystemJS.amdDefine('getlibs/plugins/typescript', ['getlibs/utils/worker'], funct
 		options = SystemJS.typescriptOptions;
 
 
+	function error(items, address, source){
+
+		var item = items[0],
+			parts = source.substr(0, item.start).split('\n'),
+			line = parts.length,
+			message = item.messageText + ' (line: ' + line + ')\n\n' +
+				source.split('\n')[line-1] + '\n' +
+				parts[line-1].replace(/\S/g, ' ') + '^';
+
+		return new Error(message);
+	}
+
+
 	function translate(load){
 		return worker.call('translate', load.address, load.source, options).then(function(result){
+
+			if (result.diagnostics.length){
+				throw error(result.diagnostics, load.address, load.source);
+			}
+
 			load.source = result.outputText.replace(/^\/\/# sourceMappingURL=.+/m, '');
 			load.metadata.sourceMap = JSON.parse(result.sourceMapText);
 			return load.source;
